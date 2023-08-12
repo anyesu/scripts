3c39ef1098e91e015555788e70e1f44cd012baab
anyesu <iewgggg@qq.com>
2023-08-12 12:00:00 +0800

    fix(cmd): :bug: missing `endlocal` in `proxy.bat`

--- cmd/proxy.bat
+++ cmd/proxy.bat
@@ -2,11 +2,11 @@
 
 set "proxy=%~1"
 if "%proxy%" == "" set "proxy=socks5://127.0.0.1:1080"
-set all_proxy=%proxy%
-set http_proxy=%proxy%
-set https_proxy=%proxy%
+endlocal & set all_proxy=%proxy%
+set http_proxy=%all_proxy%
+set https_proxy=%all_proxy%
 
-echo The proxy has been set to "%proxy%"
+echo The proxy has been set to "%all_proxy%"
 echo;
 
 curl -L cip.cc
